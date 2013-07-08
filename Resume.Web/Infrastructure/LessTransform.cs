7d59ad9db4fcb1d9d05fc6a1b73a8024476c0244
Mark geller <markg@condukia.com>
2013-07-07 19:56:38 -0500

    Set up dotless parsing

---
+++ Resume.Web/Infrastructure/LessTransform.cs
@@ -0,0 +1,14 @@
+﻿using dotless.Core;
+using System.Web.Optimization;
+
+namespace Resume.Web.Infrastructure
+{
+    public class LessTransform : IBundleTransform
+    {
+        public void Process(BundleContext context, BundleResponse response)
+        {
+            response.Content = Less.Parse(response.Content);
+            response.ContentType = "text/css";
+        }
+    }
+}
